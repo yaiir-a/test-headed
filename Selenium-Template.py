9386c0a03acbfa021b30385c591e65a847f3fd1b
Yahia <42359972+yaiir-a@users.noreply.github.com>
2024-05-16 23:04:53 +0100

    use https github

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -33,7 +33,7 @@ for option in options:
     
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get('http://github.com')
+driver.get('https://github.com')
 print(driver.title)
 with open('./GitHub_Action_Results.txt', 'w') as f:
     f.write(f"This was written with a GitHub action {driver.title}")
